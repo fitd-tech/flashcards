607a8e9e7281aceba0c4202cfefa0bf7bee4bda8
Anthony Cook <tony@fitd.tech>
2026-05-05 22:48:54 -0500

    Adjust card vertical size for small screens

--- app/deck/[slug]/page.tsx
+++ app/deck/[slug]/page.tsx
@@ -274,10 +274,10 @@ export default function Page() {
       >
         <ArrowLeftIcon />
       </Button>
-      <div className="max-h-svh w-dvw md:w-2/3 lg:w-1/2 pt-2 pb-2">
+      <div className="max-h-[calc(100svh-20px)] w-dvw md:w-2/3 lg:w-1/2">
         <Card
           onClick={handleFlipCard}
-          className={`flex flex-col min-h-svh md:min-h-96 max-h-svh items-center justify-center-safe w-full h-full hover:border cursor-pointer p-4 ${currentSide === BACK ? "bg-(--card-back)" : ""} overflow-y-auto`}
+          className={`flex flex-col min-h-[calc(100svh-20px)] md:min-h-96 max-h-[calc(100svh-20px)] items-center justify-center-safe w-full h-full hover:border cursor-pointer p-4 ${currentSide === BACK ? "bg-(--card-back)" : ""} overflow-y-auto`}
         >
           {renderCardContent()}
         </Card>
