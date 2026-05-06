c787e5b2912c296a6f4e97b38c4055fb33d242d9
Anthony Cook <tony@fitd.tech>
2026-05-05 21:53:46 -0500

    Attempt fixing page size for mobile Chrome

--- app/deck/[slug]/page.tsx
+++ app/deck/[slug]/page.tsx
@@ -241,7 +241,7 @@ export default function Page() {
       <div className="max-h-svh w-dvw md:w-2/3 lg:w-1/2 pt-2 pb-2">
         <Card
           onClick={handleFlipCard}
-          className={`flex flex-col min-h-svh sm:min-h-96 max-h-svh items-center justify-center-safe w-full h-full hover:border cursor-pointer p-4 ${currentSide === BACK ? "bg-(--card-back)" : ""} overflow-y-auto`}
+          className={`flex flex-col min-h-svh md:min-h-96 max-h-svh items-center justify-center-safe w-full h-full hover:border cursor-pointer p-4 ${currentSide === BACK ? "bg-(--card-back)" : ""} overflow-y-auto`}
         >
           {renderCardContent()}
         </Card>
